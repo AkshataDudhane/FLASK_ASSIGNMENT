f59e64612a6eb307a393d34f4426b2419869522f
akshata <d.akshata4501@gmail.com>
2023-09-05 12:39:17 +0530

    changes committed

--- orders_app.py
+++ orders_app.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from orders_model import OrdersModel
 from orders_service import Order
 from flask import *
-from mongoengine.errors import DoesNotExist
 
 
 app = Flask(__name__)
@@ -38,125 +37,67 @@ def create_order():
             return jsonify(error_response), 400
 
     # Validate input data using Order class methods
-    order = Order(order_id=id,name=data['name'], birthday=data.get('birthday'), email=data['email'], state=data['state'], zipcode=data['zipcode']) 
+    order_service = Order(order_id=id,name=data['name'], birthday=data.get('birthday'), email=data['email'], state=data['state'], zipcode=data['zipcode']) 
     
-    validation_result = order.validate_order()
+    validation_result = order_service.validate_order()
     if validation_result:
         return jsonify(validation_result), 400
-    # Create a new OrdersModel instance
-    order_model = OrdersModel(
-        name=order.user.name,
-        birthday=order.user.birthday,
-        email=order.user.email,
-        state=order.user.state,
-        zipcode=order.user.zipcode
-    )
 
-    # Save the order to the database
-    order.save_order()
-    # Prepare the response data
-    order_dict = order_model.to_dict()
+    order_service.save_order()
+    order_dict = order_service.to_dict()
 
     return jsonify({"message": "order created successfully", "response": order_dict}), 201
 
 
 @app.route('/order/<order_id>', methods=['DELETE'])
-def delete_order(order_id):
-    try:
-        order = OrdersModel.objects(id=str(order_id)).get()
-        if not order:
-            return jsonify({
-        "error_code" : "404",
-        "error" : "ORDER_NOT_FOUND",
-        "message" : "Order does not exists.",
-    }
-    ), 404
-        
-        order.update(set__is_deleted=True, set__del_time=datetime.utcnow())
-        
-        return jsonify({"is_deleted":order.is_deleted,"message": "Order marked as deleted"}), 200
-    except DoesNotExist:
-        return jsonify({"error_code": "404", "message": "Order not found", "error": "ORDER_NOT_FOUND"}), 404
-   
-    except Exception as e:
-         return jsonify({"error_code": "500", "message": "Internal Server Error", "error": str(e)}), 500
+def delete_order_route(order_id):
+    response_data, status_code = Order.delete_order(order_id)
+    if response_data is None:
+        return jsonify({
+            "error_code": "404",
+            "message": "Order not found",
+            "error": "ORDER_NOT_FOUND"
+        }), 404
+    return jsonify(response_data), status_code
 
-@app.route('/order/<order_id>', methods=['GET']) #get order by id
-def get_order(order_id):
-    try:
-        order = OrdersModel.objects(id=order_id).first()
 
-        if not order:
-            return jsonify({"error_code": "404", "message": "Order is not found", "error": "ORDER_NOT_FOUND"}), 404
-
-        if order.is_deleted:
-            return jsonify({"error_code": "404", "message": "Can't fetch deleted order"}), 404
-
-        order_dict = order.to_dict()
-        return jsonify({"response": order_dict}), 200
-
-    except DoesNotExist:
-        return jsonify({"error_code": "404", "message": "Order not found", "error": "ORDER_NOT_FOUND"}), 404
-    except Exception as e:
-        # Handle other exceptions, log them, and return an appropriate response.
-        return jsonify({"error_code": "500", "message": "Internal Server Error", "error": str(e)}), 500
+@app.route('/order/<order_id>', methods=['GET'])
+def get_order_route(order_id):
+    response_data, status_code = Order.get_order(order_id)
+    if response_data is None:
+        return jsonify({
+            "error_code": "404",
+            "message": "Order not found",
+            "error": "ORDER_NOT_FOUND"
+        }), 404
+    return jsonify({"response": response_data}), status_code
 
 
 
 @app.route('/orders', methods=['GET'])
-def list_orders():
+def list_orders_route():
     try:
         page = int(request.args.get('page', 1))
         per_page = 10
         filter_email = request.args.get('email')
         filter_state = request.args.get('state')
         filter_zipcode = request.args.get('zipcode')
-        sort_order = int(request.args.get('sort_order', 1))  # 1 for ascending, -1 for descending
+        sort_order = int(request.args.get('sort_order', 1))
 
-        query = OrdersModel.objects(is_deleted=False)
+        response_data, status_code = Order.list_orders(
+            page, per_page, filter_email, filter_state, filter_zipcode, sort_order
+        )
 
-        if filter_email:
-            query = query.filter(email=filter_email)
-        if filter_state:
-            query = query.filter(state=filter_state)
+        return jsonify(response_data), status_code
 
-        if filter_zipcode:
-            query = query.filter(zipcode=filter_zipcode)
-        total_orders = query.count()
-        start_index=per_page * (page - 1)
-        end_index=per_page * page
-        if sort_order == 1:
-                orders = query.order_by('created_time')[start_index:end_index]
-        elif sort_order == -1:
-                orders = query.order_by('-created_time')[start_index:end_index]
-        if not orders:
-                return jsonify({"message": "No orders found"}), 200
-
-        orders_list = []
-
-        for order in orders:
-            order_dict = order.to_dict()
-            orders_list.append(order_dict)
-
-        response_data = {
-            "orders": orders_list,
-            "total_orders": total_orders,
-            "page": page,
-            "per_page": per_page
-        }
-
-        return jsonify(response_data), 200
     except Exception as e:
-         return jsonify({"error_code": "500", "message": "Internal Server Error", "error": str(e)}), 500
-        
+        return jsonify({"error_code": "500", "message": "Internal Server Error", "error": str(e)}), 500
 
 @app.route('/order/<order_id>', methods=['PUT'])
 def mark_orderas_delivered(order_id):
     try:
-        # Create an instance of the OrdersModel class
         order_service = OrdersModel()
 
-        # Call the mark_order_delivered method
         response, status_code = order_service.mark_order_delivered(order_id)
 
         return jsonify(response), status_code
--- orders_service.py
+++ orders_service.py
@@ -1,11 +1,17 @@
 from orders_model import OrdersModel
 from user_service import User
 from flask import *
+from datetime import datetime
+from mongoengine.errors import DoesNotExist
+
 class Order:
     
     def __init__(self, order_id, name, email, state, zipcode,birthday=None):
         self.order_id = order_id
         self.user=User(name,birthday,email,state,zipcode)
+        self.created_time = datetime.utcnow()  # Define your created_time
+        self.updated_time = datetime.utcnow() 
+        self.is_delivered = False
 
     def validate_order(self):
         validation_checks = [
@@ -28,7 +34,103 @@ class Order:
     
     def save_order(self):
         order_model = OrdersModel(name=self.user.name, birthday=self.user.birthday, email=self.user.email, state=self.user.state, zipcode=self.user.zipcode)
-        OrdersModel.save(order_model)
+        order_model.save()
 
- 
+    def to_dict(self):
+        response_data = {
+            "name": self.user.name,
+            "email": self.user.email,
+            "birthday": self.user.birthday,
+            "state": self.user.state,
+            "zipcode": self.user.zipcode,
+            "created_time": self.created_time,
+            "updated_time": self.updated_time,
+            "is_delivered": self.is_delivered
+        }
+        return response_data
+    
+    def delete_order(order_id):
+        try:
+            order = OrdersModel.objects(id=str(order_id)).get()
+            if not order:
+                return None, 404
+
+            order.update(set__is_deleted=True, set__del_time=datetime.utcnow())
+
+            return {"is_deleted":order.is_deleted,"message": "Order marked as deleted"}, 200
+        except DoesNotExist:
+            return {"error_code": "404", "message": "Order not found", "error": "ORDER_NOT_FOUND"}, 404
+        except Exception as e:
+            return {"error_code": "500", "message": "Internal Server Error", "error": str(e)}, 500
+    def get_order(order_id):
+        try:
+            order = OrdersModel.objects(id=order_id).first()
+
+            if not order:
+                return {
+                    "error_code": "404",
+                    "message": "Order not found",
+                    "error": "ORDER_NOT_FOUND"
+                }, 404
+
+            if order.is_deleted:
+                return {
+                    "error_code": "404",
+                    "message": "Can't fetch deleted order"
+                }, 404
+
+            order_dict = order.to_dict()
+            return {"response": order_dict}, 200
+
+        except DoesNotExist:
+            return {
+                "error_code": "404",
+                "message": "Order not found",
+                "error": "ORDER_NOT_FOUND"
+            }, 404
+        except Exception as e:
+            # Handle other exceptions, log them, and return an appropriate response.
+            return {
+                "error_code": "500",
+                "message": "Internal Server Error",
+                "error": str(e)
+            }, 500
+
+    def list_orders(page, per_page, filter_email, filter_state, filter_zipcode, sort_order):
+        try:
+            query = OrdersModel.objects(is_deleted=False)
+
+            if filter_email:
+                query = query.filter(email=filter_email)
+            if filter_state:
+                query = query.filter(state=filter_state)
+
+            if filter_zipcode:
+                query = query.filter(zipcode=filter_zipcode)
+            total_orders = query.count()
+            start_index = per_page * (page - 1)
+            end_index = per_page * page
+            if sort_order == 1:
+                orders = query.order_by('created_time')[start_index:end_index]
+            elif sort_order == -1:
+                orders = query.order_by('-created_time')[start_index:end_index]
+            if not orders:
+                return {"message": "No orders found"}, 200
+
+            orders_list = []
+
+            for order in orders:
+                order_dict = order.to_dict()
+                orders_list.append(order_dict)
+
+            response_data = {
+                "orders": orders_list,
+                "total_orders": total_orders,
+                "page": page,
+                "per_page": per_page
+            }
+
+            return response_data, 200
+        except Exception as e:
+            return {"error_code": "500", "message": "Internal Server Error", "error": str(e)}, 500
 print("Validation successful")
